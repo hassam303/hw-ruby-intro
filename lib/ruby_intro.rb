8e28ee1df0f5e7097afcb0067fba35e58138b68e
Hassam Solano <solanomorelhs@g.cofc.edu>
2017-09-03 16:55:06 +0000

    Complete part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,42 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0 
+  arr.each do |i|
+    sum += i
+  end 
+  return sum 
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    sum = 0 
+    arr.sort!.reverse!
+    sum = arr[0] + arr[1]
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  rtnVal = false 
+  if arr.length == 0 || arr.length == 1
+    return rtnVal
+  end
+  
+  (0...arr.length).each do |i|
+    k = i+1
+    (k...arr.length).each do |j|
+      if (arr[i] + arr[j]) == n 
+        return rtnVal = true 
+      end
+    end
+  end
+  return rtnVal 
 end
 
 # Part 2
