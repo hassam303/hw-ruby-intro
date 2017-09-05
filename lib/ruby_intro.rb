548506fd27f080dfe779cef349fa9e9d0c9a68fc
Hassam Solano <solanomorelhs@g.cofc.edu>
2017-09-05 03:38:25 +0000

    Final Submission

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,100 +3,94 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
-  sum = 0 
+  sum = 0 #Pass: works on the empty array
   arr.each do |i|
-    sum += i
+    sum += i #Pass:returns correct sum
   end 
   return sum 
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
-  if arr.length == 0
-    return 0
-  elsif arr.length == 1
-    return arr[0]
-  else
-    sum = 0 
-    arr.sort!.reverse!
-    sum = arr[0] + arr[1]
-  end
+  #Pass: returns zero if array is empty 
+  return 0 unless arr.length != 0 
+  #Pass:returns value of the element if just one element
+  return arr[0] unless arr.length != 1 
+
+  sum = 0 
+  arr.sort!.reverse!
+  
+  #Pass:works even if 2 largest values are the same
+  sum = arr[0] + arr[1] 
+  
+  return sum #Pass:returns the correct sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
-  rtnVal = false 
-  if arr.length == 0 || arr.length == 1
-    return rtnVal
-  end
+  rtnVal = false #base case 
   
+  #Pass:returns false for an empty array 
+  #Pass: returns false for any single element array
+  return rtnVal if arr.length <= 1  
+
   (0...arr.length).each do |i|
-    k = i+1
-    (k...arr.length).each do |j|
-      if (arr[i] + arr[j]) == n 
-        return rtnVal = true 
+    (i+1...arr.length).each do |k|
+      
+      if (arr[i] + arr[k]) == n 
+        #Pass:returns true when any two elements sum to the second argument
+        rtnVal = true 
+        break
       end
+      
     end
   end
+  
   return rtnVal 
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
-  return "Hello, " + name
+  return "Hello, " + name #Pass:The hello method returns the correct string
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
-  if s.empty? 
-    return false
-  end
   
-  if s.chars[0] =~ /[AEIOUaeiou]/
-    return false
-  elsif !(s.chars[0] =~ /[[:alpha:]]/)
-    return false
-  else
-    return true 
-  end
+  #Pass:classifies true cases
+  return true unless s.empty? || #Pass:works on the empty string
+                     s.chars[0] !~ /[[:alpha:]]/|| #Pass:works on nonletters
+                     s.chars[0] =~ /[AEIOUaeiou]/ #Pass:classifies false cases
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
-  if s =~ /[[:alpha:]]/ || s.empty?
-    return false 
-  else 
-    if s.to_i % 4 == 0
-      return true 
-    else 
-      return false 
-    end
-  end
+  
+  return true unless s.empty? || 
+                     s =~ /[[:alpha:]]/ ||
+                      #Pass:classifies valid binary numbers
+                      #Pass:rejects invalid binary numbers
+                     s.to_i % 4 != 0 
 end
 
 # Part 3
-
 class BookInStock
-# YOUR CODE HERE
+  
   def initialize(isbn, price)
-    if isbn.empty? || price <= 0.00
-      raise ArgumentError 
-    else 
-      @isbn = isbn
-      @price = price 
-    end 
+    raise ArgumentError if  isbn.empty? || #Pass:should reject invalid ISBN number
+                            #Pass:should reject zero price
+                            #Pass:should reject negative price
+                            price <= 0.00
     
+    @isbn = isbn
+    @price = price 
+
   end 
   
-  attr_accessor :isbn
-  attr_accessor :price 
+  attr_accessor :isbn #PassAll:getters and setters - isbn
+  attr_accessor :price #PassAll:getters and setters - price
   
   
   def price_as_string
-    return "$%.2f" % @price.to_f
+    return "$%.2f" % @price.to_f #PassAll:price_as_string
   end
   
 
